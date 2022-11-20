89b8041e594c0d4eceaa9bbe87ea306a9791e49f
Jose Luis Quijada <jlquijada@hotmail.com>
2022-11-20 23:34:08 +0100

    end project

--- main.py
+++ main.py
@@ -9,6 +9,19 @@ stripped_invited_names = [element.strip("\n") for element in invited_names]
 
 # TODO: Insert the name of the reciever on the placeholder of the starting_letter.txt
 
+with open("./Input/Letters/starting_letter.txt") as f:
+    letter = f.read()
+
+
+# TODO: Save the letters in a file call 'letter_for_[name]' in the ReadyToSend folder
+
+for invited_names in stripped_invited_names:
+    with open(f"./Output/ReadyToSend/letter_for_{invited_names}", "w") as f:
+        f.write(letter.replace('[name]', invited_names))
+
+
+
+
 
 
 
